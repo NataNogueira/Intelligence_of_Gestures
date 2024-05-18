3506172a22ab3b9f7d98b1863e160bd44f073f70
Nathan Souza <naathanmarinho11@gmail.com>
2024-05-18 15:20:40 -0300

    arruma codigo

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-# Biblioteca usada
 import cv2
 
-# Inicializa a captura de vídeo para a webcam do meu notebook
-capture = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
-while(True):
-    # Captura quadro por quadro
-    ret, frame = capture.read()
+if not cap.isOpened():
+    print("Erro ao abrir a webcam")
+    exit()
 
-    # Exibe o quadro resultante
-    cv2.imshow('Webcam em tempo real', frame)
-
-    # Se 'q' for pressionado no teclado, o loop será interrompido, lembrando q não tem como fechar no x 
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Falha ao capturar imagem")
+        break
+    
+    cv2.imshow('Webcam', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Após apertar 'q' destrói a janela
-capture.release()
+cap.release()
 cv2.destroyAllWindows()
