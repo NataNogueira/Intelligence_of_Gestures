f987240616e3e84b5ba7f696255b1b74e747ed9b
Nata Nogueira <natanogueiraskt@gmail.com>
2024-05-29 02:40:58 -0300

    Adiciona ferramenta de detecção de gestos

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-## PRIMEIRA VERSÃO
-No atual momento foquei em conectar a webcam, para o desenvolvimento do projeto vou precisar me aprofundar em inteligências para reconhecimento de gestos e seus treinamentos.
+# INTELLIGENCE OF GESTURES
+## SEGUNDA VERSÃO
+`Primeira Versão` - foquei em conectar a webcam, para o desenvolvimento do projeto vou precisar me aprofundar em inteligências para reconhecimento de gestos e seus treinamentos.
+
+Agora foquei em adicionar uma ferramenta do python responsável pelo processamento de vídeo e deteccção em tempo real de gestos (Esqueci de colocar reconhecimento facial, mas nas próximas talvez tenha o commit :D). Próxima meta é (caso eu consiga por ora com a bagagem de conhecimentos que eu tenho agora) adicionar uma inteligência capaz de entender e guardar informações dos gestos.
--- main.py
+++ main.py
@@ -1,20 +1,49 @@
+# Bibliotecas
 import cv2
+import mediapipe as mp
 
-cap = cv2.VideoCapture(0)
+# Configura câmera local
+camera = cv2.VideoCapture(0) # O 0 significa a câmera padrão do ambiente
 
-if not cap.isOpened():
-    print("Erro ao abrir a webcam")
-    exit()
+# Inicia as bibliotecas de detecção de gestos
+# Será mostrado na tela através de drawing os movimentos captados
+mp_hands = mp.solutions.hands
+drawing = mp.solutions.drawing_utils
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Falha ao capturar imagem")
-        break
-    
-    cv2.imshow('Webcam', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# Inicia modelo de detecção
+with mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
 
-cap.release()
+
+    # Inicia Câmera
+    if not camera.isOpened():
+        print("Erro ao abrir a webcam")
+        exit()
+
+    while True:
+        success, image = camera.read()
+        if not success:
+            print("Falha ao capturar imagem")
+            break
+        
+        # Aqui começa a detecção de gestos
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        result = hands.process(image)
+
+        # Drawing(Pintura) na tela mostrando resultado
+        # Obs o resultado fica na mão mesmo
+        # Não descobri uma forma do delay ser menor
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if result.multi_hand_landmarks:
+            for hand_landmarks in result.multi_hand_landmarks:
+                drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        cv2.imshow('Webcam', image)
+        
+        # Dá um 'Close' na câmera após pressionar botão '0'
+        if cv2.waitKey(1) & 0xFF == ord('0'):
+            break
+
+# Destrói Janela
+camera.release()
 cv2.destroyAllWindows()
